819fc601411bffa589352873e569a8e9cc31170f
siddharthmallik <malliksiddharth@gmail.com>
2020-10-13 08:10:59 +0530

    commit

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.header').height($(window).height());
+})
+.header {
+  background-image: url('header-background.jpg');
+  <img class="shadow-2 aligncenter wp-image-21322" src="https://websitesetup.org/wp-content/uploads/2020/09/bootstrap-tutorial-include-header-background-imag.png" alt="bootstrap tutorial include header background image" width="750" height="382">
+  background-size: cover;
+  background-position: center;
+  position: relative;
+}
